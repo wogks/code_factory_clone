adcf9e08624eba783981db311703cacec52dbe8f
wogks <wogks27@naver.com>
2023-05-03 23:24:48 +0900

    Feat: ratingmodel작업

---
+++ lib/common/rating/model/rating_model.dart
@@ -0,0 +1,27 @@
+import 'package:codfac/common/utils/data_utils.dart';
+import 'package:codfac/user/model/user_model.dart';
+import 'package:json_annotation/json_annotation.dart';
+part 'rating_model.g.dart';
+
+@JsonSerializable()
+class RatingModel {
+  final String id;
+  final UserModel user;
+  final int rating;
+  final String content;
+  @JsonKey(
+    fromJson: DataUtils.listPathsUrls,
+  )
+  final List<String> imgUrls;
+
+  RatingModel({
+    required this.id,
+    required this.user,
+    required this.rating,
+    required this.content,
+    required this.imgUrls,
+  });
+
+  factory RatingModel.fromJson(Map<String, dynamic> json) =>
+      _$RatingModelFromJson(json);
+}
--- lib/common/utils/data_utils.dart
+++ lib/common/utils/data_utils.dart
@@ -1,7 +1,12 @@
 import '../const/data.dart';
 
 class DataUtils {
-    static pathToUrl(String value) {
+  static String pathToUrl(String value) {
     return 'http://$ip$value';
+  }
+
+  static List<String> listPathsUrls(List<String> paths) {
+    //pathTourl에 집어넣어서 똑같은 박식으로 만들어서 리스트로 만들어서 리턴
+    return paths.map((e) => pathToUrl(e)).toList();
   }
 }
