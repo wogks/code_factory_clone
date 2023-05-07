9b333db87e198c14b1ca799b193e49dfaa0b2ee8
wogks <wogks27@naver.com>
2023-05-07 23:24:30 +0900

    Feat: restaurantprovider

--- lib/restaurant/repository/restaurant_rating_repository.dart
+++ lib/restaurant/repository/restaurant_rating_repository.dart
@@ -1,10 +1,19 @@
 import 'package:codfac/common/rating/model/rating_model.dart';
 import 'package:dio/dio.dart' hide Headers;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:retrofit/retrofit.dart';
 
+import '../../common/const/data.dart';
 import '../../common/model/cursor_pagination_model.dart';
 import '../../common/model/pagination_params.dart';
 part 'restaurant_rating_repository.g.dart';
+
+final restaurantRatingRepository =
+    Provider.family<RestaurantRatingRepository, String>((ref, id) {
+  final dio = Dio();
+  return RestaurantRatingRepository(dio,
+      baseUrl: 'http://$ip/restaurant/$id/rating');
+});
 
 //http://ip/restaurant/:rid/rating
 @RestApi()
