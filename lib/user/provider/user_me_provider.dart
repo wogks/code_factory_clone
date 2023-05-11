fb0ac493c75bd0865a12697ded3c8c9f57dc3b51
wogks <wogks27@naver.com>
2023-05-11 21:52:19 +0900

    Feat: get me api 작업하기

---
+++ lib/user/provider/user_me_provider.dart
@@ -0,0 +1,27 @@
+import 'package:codfac/common/const/data.dart';
+import 'package:codfac/user/model/user_model.dart';
+import 'package:codfac/user/repository/user_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class UserMeStateNotifier extends StateNotifier<UserModelBase?> {
+  final UserMeRepository repository;
+  final FlutterSecureStorage storage;
+  UserMeStateNotifier({
+    required this.repository,
+    required this.storage,
+  }) : super(UserModelLoading()) {
+    getMe();
+  }
+  getMe() async {
+    final refreshToken = await storage.read(key: REFRESH_TOKEN_KEY);
+    final accessToken = await storage.read(key: ACCESS_TOKEN_KEY);
+
+    if (refreshToken == null || accessToken == null) {
+      return;
+    }
+    final resp = await repository.getMe();
+
+    state = resp;
+  }
+}
